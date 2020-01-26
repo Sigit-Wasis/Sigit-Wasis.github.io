ae42b6520db5a27398fe2800f350701afa7f2e17
sigit wasis subekti <sigitghoticmetal2001@gmail.com>
2020-01-26 10:17:11 +0700

    add website embuncode

---
+++ embuncode/js/main.js
@@ -0,0 +1,27 @@
+jQuery(document).ready(function() {
+
+	"use strict"
+		$('.slider').ripples({
+  			dropRadius: 13,
+  			perturbance: 0.01,
+		});
+
+		$(".text").typed({
+			strings: ["<strong>i love </strong><strong class='primary'>coding.</strong>","<strong>and to</strong><strong class='primary'> share.</strong>"],
+			typespeed: 0,
+			loop: true,
+		});
+
+		$(window).scroll(function() {
+
+			var top = $(window).scrollTop();
+			if(top >= 60) {
+				$("nav").addClass('secondary');
+				$("nav").removeClass('navbar-inverse');
+			}
+			else 
+				if($("nav").hasClass('secondary')) {
+					$("nav").removeClass('secondary');
+				}
+		});
+});
